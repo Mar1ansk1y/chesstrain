0cc65dfcdb11d032846d9da3cc8dd11f9d95501d
sonderno <zayac3452@gmail.com>
2022-05-14 01:16:00 +0500

    исправлена структура данных

--- internal/model/filevideo/filevideo.go
+++ internal/model/filevideo/filevideo.go
@@ -2,12 +2,14 @@ package filevideo
 
 import (
 	"errors"
+	"gorm.io/gorm"
 	"time"
 )
 
 var ErrNoRecord = errors.New("video: подходящего видео не найдено")
 
 type Video struct {
+	gorm.Model
 	ID       int
 	Title    string
 	Content  []byte
